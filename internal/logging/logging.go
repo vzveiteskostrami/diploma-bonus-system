07d70ed18a5ef84100fc8fa8a09374ccc063f2fe
Олег <oleg.phone@mail.ru>
2023-10-07 12:33:55 +0300

    Попытка 12a

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -84,6 +84,7 @@ func WithLogging(h http.Handler) http.Handler {
 		aa := ContextParam{Completed: true}
 		ctx := context.WithValue(r.Context(), ContextCompletedKey, aa)
 		// точка, где выполняется внутренний хендлер
+		S().Infoln("^^^^")
 		h.ServeHTTP(&lw, r.WithContext(ctx)) // обслуживание оригинального запроса
 
 		// Since возвращает разницу во времени между start
